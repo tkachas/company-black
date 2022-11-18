fa179e4e72571ddccada643ac22b7e3d62d03b12
astkach <astkachenko.corp@gmail.com>
2022-11-18 12:55:55 +0300

    title dynamic change added

--- script/switchPage.js
+++ script/switchPage.js
@@ -3,11 +3,13 @@ let about = document.querySelector('.about');
 let product = document.querySelector('.product');
 let contacts = document.querySelector('.contacts');
 
+
 let companyButton = document.querySelector('.option-company');
 let aboutButton = document.querySelector('.option-about');
 let productButton = document.querySelector('.option-product');
 let contactsButton = document.querySelector('.option-contacts');
 
+document.title = "Company";
 about.style.display = 'none';
 product.style.display = 'none';
 contacts.style.display = 'none';
@@ -23,6 +25,8 @@ function clickCompany() {
     about.style.display = 'none';
     product.style.display = 'none';
     contacts.style.display = 'none';
+
+    document.title = "Company";
 }
 
 function clickAbout() {
@@ -30,6 +34,8 @@ function clickAbout() {
     company.style.display = 'none';
     product.style.display = 'none';
     contacts.style.display = 'none';
+
+    document.title = "About";
 }
 
 function clickProduct() {
@@ -37,9 +43,13 @@ function clickProduct() {
     company.style.display = 'none';
     about.style.display = 'none';
     contacts.style.display = 'none';
+
+    document.title = "Product";
 }
 
 function clickContacts() {
+    document.title = "Contacts";
+
     contacts.style.display = 'flex';
     company.style.display = 'none';
     about.style.display = 'none';
